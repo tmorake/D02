348464f453dd342ff61c7fd71c1a7c6b06d9e3c0
Tshepang Morake <tmorake@c4r17s8.wethinkcode.co.za>
2020-06-20 10:39:33 +0200

    ft_print_numbers.c

---
+++ ex02/ft_print_numbers.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmorake <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/20 10:36:40 by tmorake           #+#    #+#             */
+/*   Updated: 2020/06/20 10:37:26 by tmorake          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+void ft_print_numbers(void)
+{
+  char number;
+
+  number = 48;
+  while (number <= 57)
+    {
+      putchar(number);
+      number++;
+    }
+}
+int main(void)
+{
+  ft_print_numbers();
+  return 0;
+}
